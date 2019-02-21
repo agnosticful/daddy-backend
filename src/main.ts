8c1d41aecc117590707ee2c2f31f6d09408cdc3e
Kohei Asai <me@axross.io>
2019-02-20 23:05:04 -0800

    Implement first example server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,41 @@
-const main = () => console.log("hello world");
+import { IncomingMessage, ServerResponse } from "http";
+import { send } from "micro";
+import { URLSearchParams } from "url";
 
-export default main;
+const whoami = process.env.DADDY_WHOAMI || "unknown";
+
+async function main(request: IncomingMessage, response: ServerResponse) {
+  const pathname = request.url!.split("?")[0];
+  const searchParams = new URLSearchParams(
+    request.url!.includes("?")
+      ? request.url!.slice(request.url!.indexOf("?"))
+      : ""
+  );
+
+  if (request.method === "GET" && pathname === "/") {
+    if (searchParams.has("message")) {
+      return send(response, 200, {
+        data: { hello: searchParams.get("message")! },
+        errors: []
+      });
+    } else {
+      response.setHeader("location", "/?message=world");
+
+      return send(response, 302);
+    }
+  }
+
+  if (request.method === "GET" && pathname === "/about") {
+    return send(response, 200, {
+      data: { whoami },
+      errors: []
+    });
+  }
+
+  return send(response, 404, {
+    data: null,
+    errors: ["NOT_FOUND"]
+  });
+}
+
+export = main;
